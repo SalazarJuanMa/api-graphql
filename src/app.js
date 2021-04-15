b3fdc325aa9aa92cd219b80e4d315e605c9738f2
salazarjuanma <licjuanma@hotmail.com>
2021-04-15 18:20:59 -0500

    add

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import  schema  from './graphql/schema'
 import {connect} from './mongodb/database';
@@ -15,8 +16,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use("/graphql", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(
-  '/graphql',
+  '/graphql', cors(),
   graphqlHTTP({
     graphiql: process.env.GRAPHQL_VALUE,
     schema: schema,
